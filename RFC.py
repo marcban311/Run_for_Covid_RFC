c39452f3248b06c43148f51af69e553c14197c51
marcban311 <mbanski6b@gmail.com>
2020-06-30 15:52:29 +0200

    16.00

--- RFC.py
+++ RFC.py
@@ -22,28 +22,20 @@ def draw_virus(viruses):
     for virus in viruses:
         screen.blit(virus_surface,virus)
 
-def create_bat():
-    random_bat_pos = random.choice(bat_random_list)
-    new_bat = bat_surface.get_rect(center = (600, random_bat_pos))
-    return new_bat
-
-def move_bat(bats):
-    for bat in bats:
-        bat.centerx -= 1 
-    return bats
-
-def draw_bat(bats):
-    for bat in bats:
-        screen.blit(bat_surface,bat)
-
+def check_collision(viruses):
+    for virus in viruses:
+        if mario_rect.colliderect(virus):
+            print("colision")
 
 pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
 
-gravity = 0.25
+#const
+gravity = 0.5
 mario_movement = 0
 
+#import grafik
 bg_surface = pygame.image.load('object/tło.png').convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
 bg_x_pos= 0
@@ -58,21 +50,16 @@ mario_surface = pygame.transform.scale2x(mario_surface)
 mario_rect = mario_surface.get_rect(center = (100, mario_minimum_y))
 
 virus_surface = pygame.image.load('object/corona.png').convert_alpha()
-virus_surface = pygame.transform.scale2x(virus_surface)
+#virus_surface = pygame.transform.scale2x(virus_surface)
 
 bat_surface = pygame.image.load('object/bat.png').convert_alpha()
 bat_surface = pygame.transform.scale2x(bat_surface)
-virus_random_list= [850, 860, 810, 750, 720, 700, 800]
+virus_random_list= [850, 860, 750, 700, 800]
 virus_y_pos_list = []
 
-bat_random_list= [710, 690, 730]
-bat_y_pos_list = []
 
 SPAWNVIRUS = pygame.USEREVENT
 pygame.time.set_timer(SPAWNVIRUS,1200)
-
-SPAWNBAT = pygame.USEREVENT
-pygame.time.set_timer(SPAWNBAT, 8000)
 
 while True:
     for event in pygame.event.get():
@@ -88,13 +75,10 @@ while True:
         if event.type == SPAWNVIRUS:
             virus_y_pos_list.append(create_virus())
 
-        if event.type == SPAWNBAT:
-            bat_y_pos_list.append(create_bat())
-
 
     bg_x_pos -= 0.5
     draw_bg()
-    floor_x_pos -= 1
+    floor_x_pos -= 5
     draw_floor()
 
 # mario i grawitacja
@@ -108,9 +92,8 @@ while True:
     virus_y_pos_list = move_virus(virus_y_pos_list)
     draw_virus(virus_y_pos_list)
 
-    bat_y_pos_list = move_bat(bat_y_pos_list)
-    draw_bat(bat_y_pos_list)
-
+#kolizja
+    check_collision(virus_y_pos_list)
    
 #zapętlenie tła 
     if bg_x_pos<= -576:
@@ -120,4 +103,4 @@ while True:
         floor_x_pos = 0
 
     pygame.display.update()
-    clock.tick(120)
+    clock.tick(60)
