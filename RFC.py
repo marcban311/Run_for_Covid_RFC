0163e7547e9def9a2ecd1327807e826a816b69ae
marcban311 <mbanski6b@gmail.com>
2020-06-30 19:40:39 +0200

    19.40

--- RFC.py
+++ RFC.py
@@ -15,7 +15,7 @@ def create_virus():
 
 def move_virus(viruses):
     for virus in viruses:
-        virus.centerx -= 5 
+        virus.centerx -= 5
     return viruses
 
 def draw_virus(viruses):
@@ -23,17 +23,24 @@ def draw_virus(viruses):
         screen.blit(virus_surface,virus)
 
 def check_collision(viruses):
+    colision= 0
     for virus in viruses:
         if mario_rect.colliderect(virus):
-            print("colision")
+            return False    
+    return True
+
+def mario_animation():
+    new_mario= mario_frames[mario_index]
+    new_mario_rect = new_mario.get_rect(center=(100, mario_rect.centery))
+    return new_mario, new_mario_rect
 
 pygame.init()
 screen = pygame.display.set_mode((576,1024))
 clock = pygame.time.Clock()
-
 #const
-gravity = 0.5
+gravity = 0.4
 mario_movement = 0
+game_activ = True
 
 #import grafik
 bg_surface = pygame.image.load('object/tło.png').convert()
@@ -44,19 +51,32 @@ floor_surface = pygame.image.load('object/podłoga.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_pos = 0
 mario_minimum_y = 861
+virus_surface = pygame.image.load('object/corona.png').convert_alpha()
 
-mario_surface = pygame.image.load('object/pic2.png').convert_alpha()
-mario_surface = pygame.transform.scale2x(mario_surface)
+mario1_surface = pygame.image.load('object/pic2aaaa.png').convert_alpha()
+mario1_surface = pygame.transform.scale2x(mario1_surface)
+
+mario2_surface = pygame.image.load('object/pic2aa.png').convert_alpha()
+mario2_surface = pygame.transform.scale2x(mario2_surface)
+
+mario3_surface = pygame.image.load('object/pic2aaa.png').convert_alpha()
+mario3_surface = pygame.transform.scale2x(mario3_surface)
+
+mario_frames = [mario1_surface,mario2_surface,mario3_surface]
+mario_index = 0 
+
+mario_surface= mario_frames[mario_index]
 mario_rect = mario_surface.get_rect(center = (100, mario_minimum_y))
 
-virus_surface = pygame.image.load('object/corona.png').convert_alpha()
-#virus_surface = pygame.transform.scale2x(virus_surface)
+MARIOSTEP = pygame.USEREVENT + 1
+pygame.time.set_timer(MARIOSTEP,50)
+
 
 bat_surface = pygame.image.load('object/bat.png').convert_alpha()
 bat_surface = pygame.transform.scale2x(bat_surface)
-virus_random_list= [850, 860, 750, 700, 800]
-virus_y_pos_list = []
 
+virus_random_list= [850, 860, 800, 830, 810, 790, 780, 870, 750]
+virus_y_pos_list = []
 
 SPAWNVIRUS = pygame.USEREVENT
 pygame.time.set_timer(SPAWNVIRUS,1200)
@@ -66,41 +86,53 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+    
         if event.type == pygame.KEYDOWN:
-            if event.key== pygame.K_SPACE:
+            if event.key== pygame.K_SPACE and game_activ:
                 mario_movement = 0
                 mario_movement -=12
+            if event.key== pygame.K_SPACE and game_activ == False:
+                game_activ= True
+                virus_y_pos_list.clear()
+                mario_rect.center = (100, mario_minimum_y)
         
         if event.type == SPAWNVIRUS:
             virus_y_pos_list.append(create_virus())
 
+        if event.type == MARIOSTEP:
+            if mario_index <2:
+                mario_index +=1
+            else:
+                mario_index = 0
 
+            mario_surface, mario_rect = mario_animation()
+    
     bg_x_pos -= 0.5
     draw_bg()
     floor_x_pos -= 5
     draw_floor()
 
-# mario i grawitacja
-    mario_movement += gravity 
-    mario_rect.centery += mario_movement
-    screen.blit(mario_surface,mario_rect)
+    if game_activ:
+    # mario i grawitacja
+        mario_movement += gravity 
+        mario_rect.centery += mario_movement
+        screen.blit(mario_surface,mario_rect)
+        game_activ = check_collision(virus_y_pos_list)
+        if mario_rect.centery >= mario_minimum_y:
+            mario_movement= 0
 
-    if mario_rect.centery >= mario_minimum_y:
-        mario_movement= 0
-#virus
-    virus_y_pos_list = move_virus(virus_y_pos_list)
-    draw_virus(virus_y_pos_list)
+    #virus
+        virus_y_pos_list = move_virus(virus_y_pos_list)
+        draw_virus(virus_y_pos_list)
 
-#kolizja
-    check_collision(virus_y_pos_list)
-   
+    #kolizja
+        check_collision(virus_y_pos_list)
+    
 #zapętlenie tła 
     if bg_x_pos<= -576:
         bg_x_pos = 0
 
     if floor_x_pos<= -576:
         floor_x_pos = 0
-
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(120)
